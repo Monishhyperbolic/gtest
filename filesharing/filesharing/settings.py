148c57ec4ad6420e508f658dfeb1be3b919a8eda
Monish Patil <161637813+Monishhyperbolic@users.noreply.github.com>
2025-07-06 12:33:16 +0000

    main

--- filesharing/filesharing/settings.py
+++ filesharing/filesharing/settings.py
@@ -126,3 +126,7 @@ MEDIA_ROOT = BASE_DIR/'static'/'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['*']  
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
