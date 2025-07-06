f89c2a967814a5bf0f442f0a7986552169df1b2a
Monish Patil <161637813+Monishhyperbolic@users.noreply.github.com>
2025-07-06 13:10:16 +0000

    main

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -o errexit
 
+# Install dependencies from root
 pip install -r requirements.txt
+
+# Move into project directory to run commands
+cd filesharing
+
+# Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
